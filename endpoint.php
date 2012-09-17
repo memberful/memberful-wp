3ab367f0e0ab659457e70db5cc6d924a3ded5515
Matt Button <matthew@sigswitch.com>
2012-09-17 11:22:42 +0100

    Fixing up the wordpress plugin to correctly validate the webhook digest

--- endpoint.php
+++ endpoint.php
@@ -5,12 +5,31 @@ define( 'MEMBERFUL_DIR', dirname( __FILE__ ) );
 if ( $_SERVER['REQUEST_METHOD'] !== 'POST' )
 	die( 'The webhook can only be accessed via POST' );
 
+
+
 $body = file_get_contents( 'php://input' );
 
+
 require_once MEMBERFUL_DIR.'/../../../wp-load.php';
+require_once MEMBERFUL_DIR.'/src/webhook_ping.php';
 
 $digest = $_SERVER['HTTP_X_MEMBERFUL_WEBHOOK_DIGEST'];
 
-$pinger = new Memberful_Wp_Ping( get_option( 'memberful_webhook_secret' ) );
+$pinger = new Memberful_Wp_Webhook_Ping( get_option( 'memberful_webhook_secret' ) );
 
-$pinger->handle_ping( $body, $digest );
+try {
+	$pinger->handle_ping( $body, $digest );
+} catch (Memberful_Wp_Ping_Invalid_Digest $e) {
+	header("Status: 401 Unauthorized");
+	echo "Digest could not be validated\n";
+	echo $e->getMessage();
+	die();
+} catch (Memberful_Wp_Ping_Invalid_Payload $e) {
+	header('Status: 400 Bad Request');
+	echo "Payload could not be parsed\n";
+	echo $e->getMessage();
+	die();
+}
+
+echo "ok";
+die();
--- src/webhook_ping.php
+++ src/webhook_ping.php
@@ -3,13 +3,11 @@
 /**
  * Handles processing an incoming webhook ping
  */
-class Memberful_Wp_Ping { 
+class Memberful_Wp_Webhook_Ping { 
 	protected $secret;
 
 	public function __construct( $secret ) { 
 		$this->secret = $secret;
-		$this->raw_payload = $raw_payload;
-		$this->payload     = json_decode( $raw_payload );
 	}
 
 	public function handle_ping( $raw_payload, $digest ) { 
@@ -30,9 +28,9 @@ class Memberful_Wp_Ping {
 	}
 
 	protected function validate_digest( $raw_payload, $digest ) { 
-		$calculated_digest = hash( 'sha256', $raw_payload, TRUE );
+		$calculated_digest = hash( 'sha256', $raw_payload.$this->secret );
 
-		if ( $calculated_digest !== $digest ) { 
+		if ( $calculated_digest != $digest ) { 
 			throw new Memberful_Wp_Ping_Invalid_Digest( $digest, $calculated_digest );
 		}
 	}
@@ -45,12 +43,22 @@ class Memberful_Wp_Ping {
 
 }
 
+class Memberful_Wp_Ping_Invalid_Digest extends RuntimeException { 
+	public function __construct($expected_digest, $calculated_digest) { 
+		parent::__construct("Expected digest ".$expected_digest." got ".$calculated_digest);
+	}
+}
+
+class Memberful_Wp_Ping_Invalid_Payload extends RuntimeException {
+
+}
+
 add_action( 'memberful_order_created', 'memberful_wp_hook_order_created' );
 
 /**
  * Triggered when a order_created event is received via webhook ping
  */
 function memberful_wp_hook_order_created( $data ) { 
-	$mapper = Memberful_User_Map( $data->order->member );
-	$mapper->map();
+	$mapper = new Memberful_User_Map( $data->order->member );
+	$mapper->map($data->order->member);
 }
