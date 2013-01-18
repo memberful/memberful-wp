2bed0ea2fc58fdc52997ef31ca192f6ba62de658
Drew Strojny <dstrojny@jestro.com>
2013-01-17 21:32:23 -0700

    Final UI polish on the set up view. Refs #32.

--- views/setup.php
+++ views/setup.php
@@ -6,15 +6,15 @@
 <div id="memberful-wrap" class="wrap">
 	<div id="memberful-registration">
 		<div class="memberful-sign-up">
-			<h1><?php _e( 'A Memberful account is required for setup', 'memberful' ); ?></h1>
-			<p><?php _e( '<a href="http://memberful.com">Sign up for an account</a> and start selling digital products and subscriptions the easy way.', 'memberful' ); ?></p>
+			<h1><?php _e( "You'll need a Memberful account.", 'memberful' ); ?></h1>
+			<p><?php _e( '<a href="http://memberful.com">Sign up for an account</a> to start selling digital goods and content the easy way.', 'memberful' ); ?></p>
 		</div>
 		<div class="memberful-register-plugin">
-			<h3><?php _e( 'Already have a Memberful account?', 'memberful' ); ?></h3>
+			<h3><?php _e( 'Already have an account?', 'memberful' ); ?></h3>
 			<form method="POST" action="<?php echo admin_url('admin.php?page=memberful_options&noheader=true') ?>">
 				<fieldset>
 					<textarea placeholder="<?php echo esc_attr( __( 'Paste your WordPress registration key here...', 'memberful' ) ); ?>" name="activation_code"></textarea>
-					<button class="button action"><?php _e( 'Connect this site to your Memberful account', 'memberful' ); ?></button>
+					<button class="button button-primary button-large"><?php _e( 'Connect to Memberful!', 'memberful' ); ?></button>
 					<input type="hidden" name="action" value="register" />
 					<?php wp_nonce_field( 'memberful_register' ); ?>
 				</fieldset>
