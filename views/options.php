35b79dbcf2a4763b5172a326597e40fcb98ee1f2
Drew Strojny <dstrojny@jestro.com>
2013-01-17 21:54:26 -0700

    Clean up UI for the options view. Refs #32.

--- views/options.php
+++ views/options.php
@@ -4,14 +4,14 @@
 	</div>
 	<div id="memberful-registered">
 		<h1><?php _e( 'Integration Active', 'memberful' ); ?></h1>
-		<p><?php printf( __( 'Managing %d products, and %d subscriptions.', 'memberful' ), count( $products ), count( $subscriptions ) ); ?></p>
-		<p><?php printf( __( 'Use the <a href="%s">Memberful Admin Panel</a> to manage your members.' ), memberful_url( 'admin' ) ) ?></p>
+		<p><?php printf( __( 'Automatically syncing %d products and %d subscriptions.', 'memberful' ), count( $products ), count( $subscriptions ) ); ?></p>
+		<p><?php printf( __( '<a href="%s">Sign in to your Memberful account</a> to manage products, subscriptions, members, and orders.' ), memberful_url( 'admin' ) ) ?></p>
 		<form method="POST" action="<?php echo admin_url('admin.php?page=memberful_options&noheader=true'); ?>">
-			<button type="submit" name="manual_sync" class="memberful-button-grey"><?php _e( 'Run Manual Sync', 'memberful' ); ?></button>
-			<button type="submit" name="reset_plugin" class="memberful-button-grey"><?php _e( 'Reset plugin', 'memberful' ); ?></button>
+			<button type="submit" name="manual_sync" class="button action"><?php _e( 'Manual sync', 'memberful' ); ?></button>
+			<button type="submit" name="reset_plugin" class="button action"><?php _e( 'Disconnect', 'memberful' ); ?></button>
 		</form>
 	</div>
 	<div class="memberful-protect-help">
-		<?php _e( "To protect individual posts and pages based on a member's products or subscriptions, edit the post or page and look for the Memberful meta box.", 'memberful' ); ?>
+		<?php _e( "To protect individual posts and pages, edit the post or page and look for the Memberful meta box below the editor.", 'memberful' ); ?>
 	</div>
 </div>
