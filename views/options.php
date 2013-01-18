dc73ae047ab93eccdbc6faf6e55a7e63c2f86da1
Drew Strojny <dstrojny@jestro.com>
2013-01-18 10:55:07 -0700

    Style up the disconnect button so the destructive action is clear and make final styling pass. Refs #32. Closes #36.

--- views/options.php
+++ views/options.php
@@ -4,11 +4,11 @@
 	</div>
 	<div id="memberful-registered">
 		<h1><?php _e( 'Integration Active', 'memberful' ); ?></h1>
-		<p><?php printf( __( 'Automatically syncing %d products and %d subscriptions.', 'memberful' ), count( $products ), count( $subscriptions ) ); ?></p>
+		<h2><?php printf( __( 'Syncing %d products and %d subscriptions.', 'memberful' ), count( $products ), count( $subscriptions ) ); ?></h2>
 		<p><?php printf( __( '<a href="%s">Sign in to your Memberful account</a> to manage products, subscriptions, members, and orders.' ), memberful_url( 'admin' ) ) ?></p>
 		<form method="POST" action="<?php echo admin_url('admin.php?page=memberful_options&noheader=true'); ?>">
-			<button type="submit" name="manual_sync" class="button action"><?php _e( 'Manual sync', 'memberful' ); ?></button>
-			<button type="submit" name="reset_plugin" class="button action"><?php _e( 'Disconnect', 'memberful' ); ?></button>
+			<button type="submit" name="manual_sync" class="button action"><?php _e( 'Run manual sync', 'memberful' ); ?></button>
+			<button type="submit" name="reset_plugin" class="button memberful-red-button action"><?php _e( 'Disconnect', 'memberful' ); ?></button>
 		</form>
 	</div>
 	<div class="memberful-protect-help">
