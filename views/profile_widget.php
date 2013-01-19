101abe388c89974d83b059c2c6fc0ef32c087ba6
Drew Strojny <dstrojny@jestro.com>
2013-01-19 12:27:43 -0700

    Add styling for profile widget. Closes #35.

--- views/profile_widget.php
+++ views/profile_widget.php
@@ -1,7 +1,9 @@
 <?php if ( is_user_logged_in() ): ?>
-	<?php echo get_avatar( wp_get_current_user()->user_email, 48 ); ?>
-	<div class="memberful-profile">
-		<div class="memberful-name"><?php echo wp_get_current_user()->display_name;  ?></div>
+	<div class="memberful-profile-gravatar">
+		<?php echo get_avatar( wp_get_current_user()->user_email, 48 ); ?>
+	</div>
+	<div class="memberful-profile-info">
+		<div class="memberful-profile-name"><?php echo wp_get_current_user()->display_name;  ?></div>
 		<div class="memberful-profile-links">
 			<a href="<?php echo memberful_member_url(); ?>" class="memberful-account-link"><?php echo __('View account'); ?></a> |
 			<a href="<?php echo memberful_signout_url(); ?>" class="memberful-sign-out-link"><?php echo __('Sign out'); ?></a>
