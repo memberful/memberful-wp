e8c003f1df55da36dc42e61033db7992f36018b7
Matt Button <matthew@sigswitch.com>
2013-01-27 16:28:44 +0000

    Ensure db migrations always run when browsing admin

--- memberful-wp.php
+++ memberful-wp.php
@@ -37,6 +37,10 @@ require_once MEMBERFUL_DIR . '/src/endpoints.php';
 
 register_activation_hook( __FILE__, 'memberful_wp_plugin_activate' );
 
+function memberful_wp_plugin_activate() {
+	add_option( 'memberful_wp_activation_redirect' , true );
+}
+
 /**
  * Get details about a specific member via the API
  *
@@ -50,6 +54,7 @@ function memberful_api_member( $member_id ) {
 	$response_body = wp_remote_retrieve_body( $response );
 
 	if ( is_wp_error( $response ) ) { 
+		echo "Couldn't contact api: ";
 		var_dump( $response, $url );
 		die();
 	}
