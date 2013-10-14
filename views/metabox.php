062734561c72560bcd7743c975355a76b92eec71
Drew Strojny <dstrojny@jestro.com>
2013-10-14 17:33:57 -0500

    Stub out new Restrict Access widget. Refs #61.

--- views/metabox.php
+++ views/metabox.php
@@ -1,38 +1,56 @@
 <?php if ( ! empty( $subscriptions ) || ! empty( $products ) ) : ?>
-	<p><?php _e( 'Restrict access by selecting from the list of products or subscriptions below.', 'memberful' ); ?></p>
+	<div class="memberful-restrict-access-options">
+		<h4 style="font-size: 1.2em; margin-bottom: 0;"><?php _e( 'Required for access', 'memberful' ); ?></h4>
+		<p><a href="#">select all</a> | <a href="#">select none</a>
+		<?php if ( ! empty( $subscriptions ) ) : ?>
+			<div id="memberful-subscriptions">
+				<ul>
+				<?php foreach($subscriptions as $id => $subscription): ?>
+					<li>
+						<label>
+							<input type="checkbox" name="memberful_subscription_acl[]" value="<?php echo $id; ?>" <?php checked( $subscription['checked'] ); ?>>
+							<?php echo esc_html( $subscription['name'] ); ?>
+						</label>
+					</li>
+				<?php endforeach; ?>
+				</ul>
+			</div>
+		<?php endif; ?>
+		<?php if ( ! empty( $products ) ) : ?>
+			<div id="memberful-products">
+				<p class="memberful-access-label"><?php _e( 'Products', 'memberful' ); ?></p>
+				<ul>
+				<?php foreach($products as $id => $product): ?>
+					<li>
+						<label>
+							<input type="checkbox" name="memberful_product_acl[]" value="<?php echo $id; ?>" <?php checked( $product['checked'] ); ?>>
+							<?php echo esc_html( $product['name'] ); ?>
+						</label>
+					</li>
+				<?php endforeach; ?>
+				</ul>
+			</div>
+		<?php endif; ?>
+	</div>
+	<div class="memberful-protected-content">
+		<?php
+
+		$content = 'This message is displayed to anyone <strong>without</strong> the required access...';
+		$editor_id = 'memberful_protected_content_message';
+		// $settings = array( 'tinymce' => false );
+
+		wp_editor( $content, $editor_id, $settings );
+
+		?>
+		<div class="memberful-restricted-access-content-description">
+			<label>
+				<input type="checkbox">
+				Make this the default message
+			</label>
+		</div>
+	</div>
 <?php else: ?>
 	<div>
 		<p><em><?php _e( "We couldn't find any products or subscriptions in your Memberful account. You'll need to add some before you can restrict access.", 'memberful' ); ?></em></p>
-	</div>
-<?php endif; ?>
-
-<?php if ( ! empty( $subscriptions ) ) : ?>
-	<div id="memberful-subscriptions">
-		<h4 style="margin-bottom: 0;"><?php _e( 'Subscriptions', 'memberful' ); ?></h4>
-		<ul>
-		<?php foreach($subscriptions as $id => $subscription): ?>
-			<li>
-				<label>
-					<input type="checkbox" name="memberful_subscription_acl[]" value="<?php echo $id; ?>" <?php checked( $subscription['checked'] ); ?>>
-					<?php echo esc_html( $subscription['name'] ); ?>
-				</label>
-			</li>
-		<?php endforeach; ?>
-		</ul>
-	</div>
-<?php endif; ?>
-<?php if ( ! empty( $products ) ) : ?>
-	<div id="memberful-products">
-		<h4 style="margin-bottom: 0;"><?php _e( 'Products', 'memberful' ); ?></h4>
-		<ul>
-		<?php foreach($products as $id => $product): ?>
-			<li>
-				<label>
-					<input type="checkbox" name="memberful_product_acl[]" value="<?php echo $id; ?>" <?php checked( $product['checked'] ); ?>>
-					<?php echo esc_html( $product['name'] ); ?>
-				</label>
-			</li>
-		<?php endforeach; ?>
-		</ul>
 	</div>
 <?php endif; ?>
