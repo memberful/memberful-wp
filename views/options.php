f57e9977e384d4b9d3e40f60d25cc71b8f305a1c
Matt Button <matthew@sigswitch.com>
2014-05-06 16:32:57 +0100

    Allow plugin to disconnect from Memberful

--- views/options.php
+++ views/options.php
@@ -8,7 +8,7 @@
 		<h2><?php printf( __( 'Syncing %d products and %d subscriptions.', 'memberful' ), count( $products ), count( $subscriptions ) ); ?></h2>
 		<p><?php printf( __( '<a href="%s">Sign in to your Memberful account</a> to manage products, subscriptions, members, and orders.' ), memberful_url( 'admin' ) ) ?></p>
 		<form method="POST" action="<?php echo admin_url('options-general.php?page=memberful_options&noheader=true'); ?>">
-			<?php memberful_wp_nonce_field( 'memberful_setup' ); ?>
+			<?php memberful_wp_nonce_field( 'memberful_options' ); ?>
 			<button type="submit" name="manual_sync" class="button action"><?php _e( 'Run manual sync', 'memberful' ); ?></button>
 			<button type="submit" name="reset_plugin" class="memberful-red-button"><?php _e( 'Disconnect', 'memberful' ); ?></button>
 		</form>
