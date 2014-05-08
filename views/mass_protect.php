858c962157de513cbf0fe7ca63ebb3ef5f92ebe0
Drew Strojny <dstrojny@jestro.com>
2014-05-08 10:40:22 -0500

    Improve styling be separating the required for access and marketing content. Also moved to a <select> for the global options. #67.

--- views/mass_protect.php
+++ views/mass_protect.php
@@ -1,6 +1,7 @@
 <div class="wrap">
 	<?php memberful_wp_render('option_tabs', array('active' => 'mass_protect')); ?>
 	<?php memberful_wp_render('flash'); ?>
+	<p>Set up global content restrictions. These settings will override any individual page restrict access settings.</p>
 	<form method="POST" action="<?php echo $form_target ?>">
 		<table class="form-table">
 			<tbody>
@@ -11,36 +12,13 @@
 							<legend class="screen-reader-text">
 								<span>Globally restrict access to</span>
 							</legend>
-							<p>
-							<label>
-								<input type="radio" class="tog" checked="checked">
-								Nothing
-							</label>
-							</p>
-							<p>
-							<label>
-								<input type="radio" class="tog">
-								Pages and Posts
-							</label>
-							</p>
-							<p>
-							<label>
-								<input type="radio" class="tog">
-								Pages
-							</label>
-							</p>
-							<p>
-							<label>
-								<input type="radio" class="tog">
-								Posts
-							</label>
-							</p>
-							<p>
-							<label>
-								<input type="radio" class="tog">
-								Specific Posts from a category
-							</label>
-							</p>
+							<select name="default_category" id="default_category" class="postform">
+								<option selected="selected">Nothing (disabled)</option>
+								<option>Pages and Posts</option>
+								<option>Pages</option>
+								<option>Posts</option>
+								<option>Posts from a category</option>
+							</select>
 							<ul class="memberful-global-restrict-access-category-list">
 								<?php foreach(get_categories() as $category): ?>
 									<li><label><input type="checkbox"  name="memberful_protect_categories[]" value="<?php echo $category->cat_ID ?>"><?php echo $category->cat_name; ?></option></label></li>
@@ -50,10 +28,56 @@
 					</td>
 				</tr>
 				<tr>
-					<th scope="row">Required for access and marketing content</th>
+					<th scope="row">Required for access</th>
 					<td>
 						<fieldset>
-							<?php memberful_wp_render( 'metabox', compact( 'subscriptions', 'products', 'marketing_content' ) ); ?>
+							<?php if ( ! empty( $subscriptions ) || ! empty( $products ) ) : ?>
+									<?php if ( ! empty( $subscriptions ) ) : ?>
+										<ul class="memberful-global-restrict-access-required-list">
+										<?php foreach($subscriptions as $id => $subscription): ?>
+											<li>
+												<label>
+													<input type="checkbox" name="memberful_subscription_acl[]" value="<?php echo $id; ?>" <?php checked( $subscription['checked'] ); ?>>
+													<?php echo esc_html( $subscription['name'] ); ?>
+												</label>
+											</li>
+										<?php endforeach; ?>
+									<?php endif; ?>
+									<?php if ( ! empty( $products ) ) : ?>
+										<?php foreach($products as $id => $product): ?>
+											<li>
+												<label>
+													<input type="checkbox" name="memberful_product_acl[]" value="<?php echo $id; ?>" <?php checked( $product['checked'] ); ?>>
+													<?php echo esc_html( $product['name'] ); ?>
+												</label>
+											</li>
+										<?php endforeach; ?>
+										</ul>
+									<?php endif; ?>
+							<?php else: ?>
+									<p><em><?php _e( "We couldn't find any products or subscriptions in your Memberful account. You'll need to add some before you can restrict access.", 'memberful' ); ?></em></p>
+							<?php endif; ?>
+						</fieldset>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">Marketing content shown to readers without access</th>
+					<td>
+						<fieldset>
+							<div class="memberful-marketing-content">
+								<?php
+
+								$editor_id = 'memberful_marketing_content';
+								$settings  = array();
+								wp_editor( $marketing_content , $editor_id, $settings );
+
+								?>
+								<div class="memberful-marketing-content-description">
+									<label>
+										<input type="checkbox" name="memberful_make_default_marketing_content" value="1">
+										Make this the default marketing content for new posts and pages
+									</label>
+								</div>
 						</fieldset>
 					</td>
 				</tr>
