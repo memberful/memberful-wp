9c585d229812a6da533115ba1863a2b0d192f3d8
Matt Button <matthew@sigswitch.com>
2014-05-11 00:09:17 +0100

    Move some methods around in api.php

--- src/api.php
+++ src/api.php
@@ -1,5 +1,76 @@
 <?php
 
+/**
+ * Get details about a specific member via the API
+ *
+ * TODO: Clean this mess up.
+ */
+function memberful_api_member( $member_id ) {
+	$response = memberful_wp_get_data_from_api(
+		memberful_wp_wrap_api_token( memberful_admin_member_url( $member_id, MEMBERFUL_JSON ) ),
+		'fetch_member_account_from_admin'
+	);
+
+	$response_code = (int) wp_remote_retrieve_response_code( $response );
+	$response_body = wp_remote_retrieve_body( $response );
+
+	if ( is_wp_error( $response ) ) {
+		echo "Couldn't contact api: ";
+		var_dump( $response, $url );
+		die();
+	}
+
+	if ( 200 !== $response_code OR empty( $response_body ) ) {
+		var_dump( $response );
+		return new WP_Error( 'memberful_fail', 'Could not get member info from api' );
+	}
+
+	return json_decode( $response_body );
+}
+
+function memberful_wp_get_data_from_api( $url, $caller ) {
+	if ( strpos($url, 'access_token') === NULL && strpos($url, 'auth_token') === NULL ) {
+		$url = memberful_wp_wrap_api_token($url);
+	}
+	$user_agent = 'WordPress/'.$wp_version.' (PHP '.phpversion().') memberful-wp/'.MEMBERFUL_VERSION;
+	$request = array(
+		'sslverify' => MEMBERFUL_SSL_VERIFY,
+		'headers'   => array(
+			'User-Agent' => $user_agent,
+			'Accept' => 'application/json'
+		),
+		'timeout'   => 15
+	);
+
+	$response = wp_remote_get( $url, $request );
+
+	memberful_wp_instrument_api_call( $url, $request, $response, $caller );
+}
+
+function memberful_wp_put_data_to_api_as_json( $url, $caller, $data ) {
+	global $wp_version;
+
+	$url        = memberful_wp_wrap_api_token( $url );
+	$user_agent = 'WordPress/'.$wp_version.' (PHP '.phpversion().') memberful-wp/'.MEMBERFUL_VERSION;
+	$request    = array(
+		'method'  => 'PUT',
+		'headers' => array(
+			'User-Agent' => $user_agent,
+			'Content-Type' => 'application/json',
+			'Accept' => 'application/json'
+		),
+		'body' => json_encode( $data ),
+		'timeout' => 15,
+		'sslverify' => MEMBERFUL_SSL_VERIFY,
+	);
+
+	$response = wp_remote_post( $url, $request );
+
+	memberful_wp_instrument_api_call( $url, $request, $response, $caller );
+
+	return $response;
+}
+
 function memberful_wp_instrument_api_call( $url, $request, $response, $caller ) {
 	$error_payload = NULL;
 
@@ -60,52 +131,3 @@ function memberful_wp_record_api_response_error( $new_payload ) {
 }
 
 
-/**
- * Get details about a specific member via the API
- *
- * TODO: Clean this mess up.
- */
-function memberful_api_member( $member_id ) {
-	$url = memberful_wp_wrap_api_token( memberful_admin_member_url( $member_id, MEMBERFUL_JSON ) );
-
-	$response	  = wp_remote_get( $url, array( 'sslverify' => MEMBERFUL_SSL_VERIFY ) );
-	$response_code = (int) wp_remote_retrieve_response_code( $response );
-	$response_body = wp_remote_retrieve_body( $response );
-
-	if ( is_wp_error( $response ) ) {
-		echo "Couldn't contact api: ";
-		var_dump( $response, $url );
-		die();
-	}
-
-	if ( 200 !== $response_code OR empty( $response_body ) ) {
-		var_dump( $response );
-		return new WP_Error( 'memberful_fail', 'Could not get member info from api' );
-	}
-
-	return json_decode( $response_body );
-}
-
-function memberful_wp_send_data_to_api_as_json( $url, $caller, $data ) {
-	global $wp_version;
-
-	$url        = memberful_wp_wrap_api_token( $url );
-	$user_agent = 'WordPress/'.$wp_version.' (PHP '.phpversion().') memberful-wp/'.MEMBERFUL_VERSION;
-	$request    = array(
-		'method'  => 'PUT',
-		'headers' => array(
-			'User-Agent' => $user_agent,
-			'Content-Type' => 'application/json',
-			'Accept' => 'application/json'
-		),
-		'body' => json_encode( $data ),
-		'timeout' => 10,
-		'sslverify' => MEMBERFUL_SSL_VERIFY,
-	);
-
-	$response = wp_remote_post( $url, $request );
-
-	memberful_wp_instrument_api_call( $url, $request, $response, $caller );
-
-	return $response;
-}
