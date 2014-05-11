335bc1e9aa799053becfdbabe623ba9fec31e108
Matt Button <matthew@sigswitch.com>
2014-05-11 01:06:04 +0100

    Fix name of referenced class in User_Downloads #77

--- src/user/downloads.php
+++ src/user/downloads.php
@@ -8,7 +8,7 @@ require MEMBERFUL_DIR.'/src/user/entity.php';
 class Memberful_Wp_User_Downloads extends Memberful_Wp_User_Entity { 
 
 	static public function sync( $user_id, $entities ) {
-		$syncer = new Memberful_Wp_User_Products($user_id);
+		$syncer = new Memberful_Wp_User_Downloads($user_id);
 		return $syncer->set($entities);
 	}
 
