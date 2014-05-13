5f966faa4db27d6c588e56704ccc7deaae0395d7
Matt Button <matthew@sigswitch.com>
2014-05-13 16:46:18 +0100

    Fix some bugs in the webhook received #72

--- src/endpoints/webhook.php
+++ src/endpoints/webhook.php
@@ -12,21 +12,29 @@ class Memberful_Wp_Endpoint_Webhook implements Memberful_Wp_Endpoint {
 	public function process( array $request_params, array $server_params ) {
 		$payload = json_decode($this->raw_request_body());
 
-		if ( strpos( $payload->event, 'order.' ) !== FALSE ) {
-			$this->sync_member( $payload->order->member->id );
-		} elseif ( strpos( $payload->event, 'member.' ) !== FALSE ) {
-			$this->sync_member( $payload->member->id );
+		if ( strpos( $payload->event, 'order' ) !== FALSE ) {
+			$member = (int) $payload->order->member->id;
+
+			echo 'Processing order webhook for member '.$member;
+			$this->sync_member( $member );
+		} elseif ( strpos( $payload->event, 'member' ) !== FALSE ) {
+			$member = (int) $payload->member->id;
+
+			echo 'Processing member webhook for member '.$member;
+			$this->sync_member( $member );
+		} else {
+			echo 'Ignoring webhook';
 		}
 	}
 
 	private function sync_member( $member_id ) {
-		$account = memberful_api_member( $member__id );
+		$account = memberful_api_member( $member_id );
 
 		$mapper = new Memberful_User_Map();
 		$user   = $mapper->map( $account->member );
 
-		Memberful_Wp_User_Products::sync( $user->ID, $details->products );
-		Memberful_Wp_User_Subscriptions::sync( $user->ID, $details->subscriptions );
+		Memberful_Wp_User_Downloads::sync( $user->ID, $account->products );
+		Memberful_Wp_User_Subscriptions::sync( $user->ID, $account->subscriptions );
 	}
 
 	private function raw_request_body() {
