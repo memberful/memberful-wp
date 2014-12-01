cb2c9c49484361b388cd91a16eb0cbd6ec8accc6
Matt Button <matthew@sigswitch.com>
2014-12-01 16:52:21 +0000

    Re-order links in editor #105

--- js/memberful-wp-tinymce-plugin.js
+++ js/memberful-wp-tinymce-plugin.js
@@ -137,10 +137,10 @@
 				type: 'menubutton',
 				text: 'Memberful',
 				menu: [
-					{text: 'Free signup link', onclick: function() { insertRegistrationShortcode(editor); }},
-					{text: 'Sign in link', onclick: function() { insertSignInShortcode(editor); }},
 					{text: 'Buy subscription plan', onclick: function() { insertSubscriptionCheckoutLink(editor); }},
 					{text: 'Buy Download', onclick: function() { insertDownloadCheckoutLink(editor); }},
+					{text: 'Free signup link', onclick: function() { insertRegistrationShortcode(editor); }},
+					{text: 'Sign in link', onclick: function() { insertSignInShortcode(editor); }},
 					{text: 'Link to Download', onClick: function() { insertLinkToDownload(editor); }},
 				]
 			});
