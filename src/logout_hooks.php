0ea0fef34ab028b428b7ede920caeda26632a258
Matt Button <matthew@sigswitch.com>
2014-12-04 23:12:07 +0000

    Ensure signing out of WordPress always signs out of Memberful fixes #87

---
+++ src/logout_hooks.php
@@ -0,0 +1,17 @@
+<?php
+
+// Try to ensure that this is the last thing done after a user is logged out
+add_action( 'wp_logout', 'memberful_wp_ensure_user_logged_out_of_memberful', 5000 );
+
+function memberful_wp_ensure_user_logged_out_of_memberful() {
+	if ( ! memberful_wp_is_connected_to_site() ) {
+		return;
+	}
+
+	if ( memberful_wp_endpoint_for_request() !== NULL ) {
+		return;
+	}
+
+	wp_safe_redirect( memberful_sign_out_url() );
+	exit();
+}
