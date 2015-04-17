15ec48113b82c31127e3a535d3cb373e71ab28e0
Andrei-Robert Rusu <rusuandreirobert@gmail.com>
2015-04-17 20:12:15 +0300

    Ensure the feed format matches the default WordPress feed exactly

--- views/private_user_feed_content.php
+++ views/private_user_feed_content.php
@@ -9,7 +9,10 @@ query_posts(array(
     'posts_per_page'  => -1
 ));
 
-echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>' ?>
+echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+
+do_action( 'rss_tag_pre', 'rss2' );
+?>
 <rss version="2.0"
      xmlns:content="http://purl.org/rss/1.0/modules/content/"
      xmlns:wfw="http://wellformedweb.org/CommentAPI/"
@@ -22,23 +25,76 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>' ?>
     <title><?php bloginfo_rss('name'); ?> Member Feed</title>
     <atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
     <link><?php bloginfo_rss('url') ?></link>
-    <description><?php bloginfo_rss('description') ?></description>
+    <description><?php bloginfo_rss("description") ?></description>
     <lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></lastBuildDate>
-    <language><?php echo get_option('rss_language', 'en-US'); ?></language>
-    <sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
-    <sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
-    <?php do_action('rss2_head'); ?>
-    <?php while(have_posts()) : the_post(); ?>
+    <language><?php bloginfo_rss( 'language' ); ?></language>
+    <sy:updatePeriod><?php
+      $duration = 'hourly';
+
+      /**
+       * Filter how often to update the RSS feed.
+       *
+       * @since 2.1.0
+       *
+       * @param string $duration The update period. Accepts 'hourly', 'daily', 'weekly', 'monthly',
+       *                         'yearly'. Default 'hourly'.
+       */
+      echo apply_filters( 'rss_update_period', $duration );
+      ?></sy:updatePeriod>
+    <sy:updateFrequency><?php
+      $frequency = '1';
+
+      /**
+       * Filter the RSS update frequency.
+       *
+       * @since 2.1.0
+       *
+       * @param string $frequency An integer passed as a string representing the frequency
+       *                          of RSS updates within the update period. Default '1'.
+       */
+      echo apply_filters( 'rss_update_frequency', $frequency );
+      ?></sy:updateFrequency>
+    <?php
+      /**
+       * Fires at the end of the RSS2 Feed Header.
+       *
+       * @since 2.0.0
+       */
+      do_action( 'rss2_head');
+
+      while( have_posts()) : the_post();
+      ?>
       <item>
-        <title><?php the_title_rss(); ?></title>
-        <link><?php the_permalink_rss(); ?></link>
+        <title><?php the_title_rss() ?></title>
+        <link><?php the_permalink_rss() ?></link>
+        <comments><?php comments_link_feed(); ?></comments>
         <pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
-        <dc:creator><?php the_author(); ?></dc:creator>
+        <dc:creator><![CDATA[<?php the_author() ?>]]></dc:creator>
+        <?php the_category_rss('rss2') ?>
+
         <guid isPermaLink="false"><?php the_guid(); ?></guid>
-        <description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
-        <content:encoded><![CDATA[<?php the_excerpt_rss() ?>]]></content:encoded>
+        <?php if (get_option('rss_use_excerpt')) : ?>
+          <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
+        <?php else : ?>
+          <description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
+          <?php $content = get_the_content_feed('rss2'); ?>
+          <?php if ( strlen( $content ) > 0 ) : ?>
+            <content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
+          <?php else : ?>
+            <content:encoded><![CDATA[<?php the_excerpt_rss(); ?>]]></content:encoded>
+          <?php endif; ?>
+        <?php endif; ?>
+        <wfw:commentRss><?php echo esc_url( get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
+        <slash:comments><?php echo get_comments_number(); ?></slash:comments>
         <?php rss_enclosure(); ?>
-        <?php do_action('rss2_item'); ?>
+        <?php
+        /**
+         * Fires at the end of each RSS2 feed item.
+         *
+         * @since 2.0.0
+         */
+        do_action( 'rss2_item' );
+        ?>
       </item>
     <?php endwhile; ?>
   </channel>
