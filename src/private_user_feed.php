dcf220a23cc48d08d1cec2965e803c143bc5d343
Andrei-Robert Rusu <rusuandreirobert@gmail.com>
2015-05-05 16:19:44 +0300

    http to https redirect issues on the feed. - attempt 1
    This is just an attempt, as there may be users that have a different
    redirect.

--- src/private_user_feed.php
+++ src/private_user_feed.php
@@ -9,7 +9,7 @@
  * @author Andrei-Robert Rusu
  */
 
-add_action('init', 'memberful_private_user_feed_init');
+add_action('init', 'memberful_private_user_feed_init', 1);
 
 function memberful_private_user_feed_init() {
 	// In case this is not available, just don't carry on with the logic, it should always be here.
