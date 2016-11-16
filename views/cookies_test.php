5809c2b5200a1716c9f38f771ecec1a988e3c49f
Patrik Bóna <patrik.bona@mrhead.sk>
2016-11-16 16:03:39 +0100

    Make URL dynamic

--- views/cookies_test.php
+++ views/cookies_test.php
@@ -3,7 +3,7 @@
   <?php memberful_wp_render('flash'); ?>
   <div class="postbox memberful-postbox">
     <p>
-      Some web hosting services may block cookies or use extensive caching. This prevents Memberful from working properly. If the cookies test fails, please ask your hosting service to allow cookies from Memberful and also ask them to disable caching of URLs that start with <code>https://yoursite.com?memberful_endpoint</code>. 
+      Some web hosting services may block cookies or use extensive caching. This prevents Memberful from working properly. If the cookies test fails, please ask your hosting service to allow cookies from Memberful and also ask them to disable caching of URLs that start with <code><?php echo get_site_url(); ?>/?memberful_endpoint</code>.
     </p>
 
     <a href="<?php echo memberful_wp_endpoint_url('set_test_cookie') ?>" class="button">Run Cookies Test</a>
