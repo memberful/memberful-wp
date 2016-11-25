64c9c4cb84bd0a8ad1fd37c4a110758d762d2bcb
Patrik Bóna <patrik.bona@mrhead.sk>
2016-11-25 10:48:33 +0100

    Add support for beaver builder
    Remove beaver builder's `the_content` filter if post is protected by
    Memberful.

--- src/content_filter.php
+++ src/content_filter.php
@@ -2,7 +2,6 @@
 
 add_action( 'the_content', 'memberful_wp_protect_content', -10 );
 
-
 function memberful_wp_protect_content( $content ) {
   global $post;
 
@@ -11,6 +10,8 @@ function memberful_wp_protect_content( $content ) {
   }
 
   if ( ! memberful_can_user_access_post( wp_get_current_user()->ID, $post->ID ) ) {
+    remove_action( "the_content", "FLBuilder::render_content" );
+
     $memberful_marketing_content = memberful_marketing_content( $post->ID );
     return apply_filters( 'memberful_wp_protect_content', $memberful_marketing_content );
   }
