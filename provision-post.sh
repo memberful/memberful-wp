4008644d044053217b03050790073449d56b212d
Patrik Bóna <patrik.bona@mrhead.sk>
2017-04-05 15:01:36 +0200

    Setup default pages, activate Memberful widget

--- provision-post.sh
+++ provision-post.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+wp() {
+  /usr/local/bin/wp --path=/vagrant/wordpress $@
+}
+
+wp_create_page() {
+  echo wp post create --post_type=page --post_status="publish" $@
+}
+
 echo "Activating Memberful WP plugin"
-/usr/local/bin/wp --path=/vagrant/wordpress plugin activate memberful-wp
+wp plugin activate memberful-wp
+wp_create_page --post_title="Checkout page" --post_content="[memberful_buy_subscription_link plan='1-monthly']Buy a plan[/memberful_buy_subscription_link]"
+wp_create_page --post_title="After sign in page"
+wp_create_page --post_title="After sign out page"
+wp widget add memberful_wp_profile_widget sidebar-1 1
 
 if ! grep "memberful.dev" /etc/hosts; then
   echo "Adding hosts entries for Memberful"
