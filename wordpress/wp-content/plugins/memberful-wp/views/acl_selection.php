8d79235649f6fdb32c369f7af7a41226c26d4fcf
Patrik Bóna <patrik.bona@mrhead.sk>
2019-08-09 09:58:41 +0200

    Update copy
    And fix a styling issue.

--- wordpress/wp-content/plugins/memberful-wp/views/acl_selection.php
+++ wordpress/wp-content/plugins/memberful-wp/views/acl_selection.php
@@ -1,7 +1,7 @@
     <div class="memberful-acl-block">
       <label>
         <input type="checkbox" name="memberful_viewable_by_any_registered_users" value="1" <?php if(isset($viewable_by_any_registered_users) && $viewable_by_any_registered_users): ?>checked="checked"<?php endif; ?> />
-        Any registered user
+        Anybody with an account
       </label>
       <label data-depends-on="memberful_viewable_by_any_registered_users" data-depends-value-not="1">
         <input type="checkbox" name="memberful_viewable_by_anybody_subscribed_to_a_plan" value="1" <?php if(isset($viewable_by_anybody_subscribed_to_a_plan) && $viewable_by_anybody_subscribed_to_a_plan): ?>checked="checked"<?php endif; ?> />
@@ -12,7 +12,7 @@
       <div data-depends-on="memberful_viewable_by_anybody_subscribed_to_a_plan" data-depends-value-not="1">
         <?php if ( ! empty( $subscriptions ) ) : ?>
           <div id="memberful-subscriptions" class="memberful-acl-block">
-            <p class="memberful-access-label"><?php _e( 'Anybody subscribed to:', 'memberful' ); ?></p>
+            <p class="memberful-access-label">Anybody subscribed to a specific plan:</p>
             <ul>
             <?php foreach($subscriptions as $id => $subscription): ?>
               <li>
@@ -27,7 +27,7 @@
         <?php endif; ?>
         <?php if ( ! empty( $products ) ) : ?>
           <div id="memberful-downloads" class="memberful-acl-block">
-            <p class="memberful-access-label"><?php _e( 'Anybody who owns:', 'memberful' ); ?></p>
+            <p class="memberful-access-label">Anybody who owns:</p>
             <ul>
             <?php foreach($products as $id => $product): ?>
               <li>
