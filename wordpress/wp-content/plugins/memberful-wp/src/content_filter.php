ebb1aecf37bed67fe8c4ca8c5af3a16d94243f74
Jacob Paul <jacob@jacobpaul.io>
2019-12-04 14:59:07 -0600

    Unhook from Elementor templates other than queried post
    Support protecting pages that include the Elementor Pro posts widget.

--- wordpress/wp-content/plugins/memberful-wp/src/content_filter.php
+++ wordpress/wp-content/plugins/memberful-wp/src/content_filter.php
@@ -14,7 +14,9 @@ function memberful_wp_protect_content( $content ) {
     remove_action( "the_content", "FLBuilder::render_content" );
 
     // Remove Elementor action hook
-    remove_action("elementor/frontend/the_content", "memberful_wp_protect_content");
+    if (get_queried_object_id() === $post->ID) {
+      remove_action("elementor/frontend/the_content", "memberful_wp_protect_content");
+    }
 
     // Remove media enclosures from the RSS feed
     add_filter("rss_enclosure", "__return_empty_string");
