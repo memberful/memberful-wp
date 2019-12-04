369b149ab1c586a7820393c9b1404087d7b85d96
Jacob Paul <jacob@jacobpaul.io>
2019-12-04 14:57:43 -0600

    Revert "Revert "Merge pull request #245 from memberful/elementor-compatibility-fix""
    This reverts commit abfb9136f86acea1a1724bd074696578f137da8d.

--- wordpress/wp-content/plugins/memberful-wp/src/contrib/elementor.php
+++ wordpress/wp-content/plugins/memberful-wp/src/contrib/elementor.php
@@ -1,3 +1,8 @@
 <?php
 
-add_action('elementor/frontend/the_content', 'memberful_wp_protect_content');
+add_filter('elementor/frontend/builder_content_data', function($data, $post_id) {
+  if (get_queried_object_id() === $post_id) {
+    add_action('elementor/frontend/the_content', 'memberful_wp_protect_content');
+  }
+  return $data;
+}, 10, 2);
