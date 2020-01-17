812e40188d290cc060571224b37ccf7d98c069fa
Lucas Mansur <lucas.mansur2@gmail.com>
2020-01-17 12:13:40 -0300

    Bump version to 1.51.0

--- wordpress/wp-content/plugins/memberful-wp/memberful-wp.php
+++ wordpress/wp-content/plugins/memberful-wp/memberful-wp.php
@@ -3,14 +3,14 @@
 Plugin Name: Memberful WP
 Plugin URI: http://github.com/memberful/memberful-wp
 Description: Sell memberships and restrict access to content with WordPress and Memberful.
-Version: 1.50.3
+Version: 1.51.0
 Author: Memberful
 Author URI: http://memberful.com
 License: GPLv2 or later
  */
 
 if ( ! defined( 'MEMBERFUL_VERSION' ) )
-  define( 'MEMBERFUL_VERSION', '1.50.3' );
+  define( 'MEMBERFUL_VERSION', '1.51.0' );
 
 if ( ! defined( 'MEMBERFUL_PLUGIN_FILE' ) )
   define( 'MEMBERFUL_PLUGIN_FILE', __FILE__ );
