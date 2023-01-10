6fb5ff33e4268b21e2161394013b39b597f68e03
julianfssen <julianfssen@gmail.com>
2023-01-10 17:35:03 +0800

    Added ES setup in docker-compose.yml. Reverted code in content_filter.php

--- wordpress/wp-content/plugins/memberful-wp/src/content_filter.php
+++ wordpress/wp-content/plugins/memberful-wp/src/content_filter.php
@@ -3,6 +3,11 @@
 add_action( 'the_content', 'memberful_wp_protect_content', 100 );
 
 function memberful_wp_protect_content( $content ) {
+  global $post;
+
+  error_log( 'DEBUGGING: Post type' );
+  error_log( gettype($post) );
+
   if(doing_filter('memberful_wp_protect_content')){
     return $content;
   }
@@ -11,25 +16,19 @@ function memberful_wp_protect_content( $content ) {
     return $content;
   }
 
-  $post_id = get_the_ID();
-
-  if (!$post_id) {
-    // TODO
-  }
-
-  if ( ! memberful_can_user_access_post( wp_get_current_user()->ID, $post_id ) ) {
+  if ( ! memberful_can_user_access_post( wp_get_current_user()->ID, $post->ID ) ) {
     // Disable Beaver Builder
     remove_action( "the_content", "FLBuilder::render_content" );
 
     // Remove Elementor action hook
-    if (get_queried_object_id() === $post_id) {
+    if (get_queried_object_id() === $post->ID) {
       remove_action("elementor/frontend/the_content", "memberful_wp_protect_content");
     }
 
     // Remove media enclosures from the RSS feed
     add_filter("rss_enclosure", "__return_empty_string");
 
-    $memberful_marketing_content = memberful_marketing_content( $post_id );
+    $memberful_marketing_content = memberful_marketing_content( $post->ID );
     return apply_filters( 'memberful_wp_protect_content', $memberful_marketing_content );
   }
 
