311b4692392b7fba3c825a7504d9ba240ae3b611
julianfssen <julianfssen@gmail.com>
2023-08-24 09:11:32 +0800

    Bump version to 1.73.3

--- wordpress/wp-content/plugins/memberful-wp/memberful-wp.php
+++ wordpress/wp-content/plugins/memberful-wp/memberful-wp.php
@@ -3,14 +3,14 @@
 Plugin Name: Memberful WP
 Plugin URI: http://github.com/memberful/memberful-wp
 Description: Sell memberships and restrict access to content with WordPress and Memberful.
-Version: 1.73.2
+Version: 1.73.3
 Author: Memberful
 Author URI: http://memberful.com
 License: GPLv2 or later
  */
 
 if ( ! defined( 'MEMBERFUL_VERSION' ) )
-  define( 'MEMBERFUL_VERSION', '1.73.2' );
+  define( 'MEMBERFUL_VERSION', '1.73.3' );
 
 if ( ! defined( 'MEMBERFUL_PLUGIN_FILE' ) )
   define( 'MEMBERFUL_PLUGIN_FILE', __FILE__ );
