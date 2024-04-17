121653bdbd1dd4ad7d9304023c08190700246c5e
Patrik Bóna <patrik.bona@mrhead.sk>
2024-04-17 09:30:24 +0200

    Keep only 10 tags and remove extraneous bracket

--- release.sh
+++ release.sh
@@ -81,12 +81,11 @@ push_to_wordpress_svn() {
   svn copy trunk tags/$VERSION
   cd tags/$VERSION
   svn commit -m "$SVN_COMMIT_MESSAGE"
-}
 
-  # Keep only the latest 20 tags
+  # Keep only the latest 10 tags
   echo "Checking and removing old tags if necessary"
   cd $SVN_LOCAL_PATH/tags
-  TAGS_TO_DELETE=$(svn ls | sort -t. -k1,1nr -k2,2nr -k3,3nr | tail -n +21)
+  TAGS_TO_DELETE=$(svn ls | sort -t. -k1,1nr -k2,2nr -k3,3nr | tail -n +11)
 
   if [ ! -z "$TAGS_TO_DELETE" ]; then
     echo "Removing old tags..."
