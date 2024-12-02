81c9e779274fe5d634463680d33643e4d326818a
Patrik Bóna <patrik.bona@mrhead.sk>
2024-12-02 17:26:00 +0100

    Fix `memberful_wp_user_disallowed_term_ids()`
    The `memberful_wp_user_disallowed_term_ids()` function does not work as
    expected. It calls `memberful_wp_user_disallowed_ids_from_acl()`, which
    works well for posts but not for terms. This is because, instead of
    identifying all terms available to any registered users or subscribers,
    it relies on posts available to registered users and subscribers (see
    `$posts_for_any_registered_users` and
    `$posts_for_anybody_subscribed_to_a_plan`).
    
    Although `memberful_wp_user_disallowed_term_ids()` is buggy, it does not
    cause real-world issues. It is called from
    `memberful_first_term_restricting_post()`, which checks for terms
    available to any registered users or subscribers. This workaround is
    necessary because `memberful_wp_user_disallowed_term_ids()` does not
    return these terms as expected.
    
    This commit fixes it by introducing _memberful_wp_items_from_acl(),
    which handles both posts and terms, ensuring consistent behavior.
    Additionally, function call caching is fixed; previously,
    `$disallowed_post_ids` was checked before being declared as `static`.
    
    I don't plan to release the on its own, so I'm not including a changelog
    entry.

--- wordpress/wp-content/plugins/memberful-wp/src/acl/post_options.php
+++ wordpress/wp-content/plugins/memberful-wp/src/acl/post_options.php
@@ -7,7 +7,7 @@ function memberful_wp_get_post_available_to_any_registered_users( $post_id ) {
 function memberful_wp_set_post_available_to_any_registered_users( $post_id, $is_viewable_by_any_registered_users ) {
   update_post_meta( $post_id, 'memberful_available_to_any_registered_user', $is_viewable_by_any_registered_users);
 
-  $globally_viewable_by_by_any_registered_users = memberful_wp_get_all_posts_available_to_any_registered_user();
+  $globally_viewable_by_by_any_registered_users = memberful_wp_posts_for_registered_users();
 
   if ( $is_viewable_by_any_registered_users ) {
     $globally_viewable_by_by_any_registered_users[$post_id] = $post_id;
@@ -18,7 +18,7 @@ function memberful_wp_set_post_available_to_any_registered_users( $post_id, $is_
   update_option( 'memberful_posts_available_to_any_registered_user', $globally_viewable_by_by_any_registered_users );
 }
 
-function memberful_wp_get_all_posts_available_to_any_registered_user() {
+function memberful_wp_posts_for_registered_users() {
   return get_option( 'memberful_posts_available_to_any_registered_user', array() );
 }
 
@@ -29,7 +29,7 @@ function memberful_wp_get_post_available_to_anybody_subscribed_to_a_plan( $post_
 function memberful_wp_set_post_available_to_anybody_subscribed_to_a_plan( $post_id, $is_viewable ) {
   update_post_meta( $post_id, 'memberful_available_to_anybody_subscribed_to_a_plan', $is_viewable );
 
-  $posts_available_to_anybody_subscribed_to_a_plan = memberful_wp_get_all_posts_available_to_anybody_subscribed_to_a_plan();
+  $posts_available_to_anybody_subscribed_to_a_plan = memberful_wp_posts_for_subscribers();
 
   if ( $is_viewable ) {
     $posts_available_to_anybody_subscribed_to_a_plan[$post_id] = $post_id;
@@ -40,6 +40,6 @@ function memberful_wp_set_post_available_to_anybody_subscribed_to_a_plan( $post_
   update_option( 'memberful_posts_available_to_anybody_subscribed_to_a_plan', $posts_available_to_anybody_subscribed_to_a_plan);
 }
 
-function memberful_wp_get_all_posts_available_to_anybody_subscribed_to_a_plan() {
+function memberful_wp_posts_for_subscribers() {
   return get_option( 'memberful_posts_available_to_anybody_subscribed_to_a_plan', array() );
 }
