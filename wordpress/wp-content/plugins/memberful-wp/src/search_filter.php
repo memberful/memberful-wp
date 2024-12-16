e9adb22b1871cdd551eaf1321699d0ac20ddd443
Patrik Bóna <patrik.bona@mrhead.sk>
2024-12-16 10:08:17 +0100

    Do not filter search results for admins
    https://3.basecamp.com/3293071/buckets/5610905/card_tables/cards/8125823003

--- wordpress/wp-content/plugins/memberful-wp/src/search_filter.php
+++ wordpress/wp-content/plugins/memberful-wp/src/search_filter.php
@@ -3,6 +3,11 @@
 add_action( 'pre_get_posts', 'memberful_wp_protect_search' );
 
 function memberful_wp_protect_search( $query ) {
+  // Do not filter search results for admins
+  if ( current_user_can( 'publish_posts' ) ) {
+    return $query;
+  }
+
   if ( !is_admin() && $query->is_search() ) {
     $disallowed_post_ids = memberful_wp_user_disallowed_post_ids( get_current_user_id() );
 
