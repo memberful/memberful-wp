fc882efb378b62de0656b3494efef0fcb26664c0
Zachary Hickson <zhickson@thecode.co>
2025-11-21 11:56:34 +1100

    Apply visibility conditions when rendering blocks

--- wordpress/wp-content/plugins/memberful-wp/js/src/blocks/extensions/block-visibility/index.js
+++ wordpress/wp-content/plugins/memberful-wp/js/src/blocks/extensions/block-visibility/index.js
@@ -1,4 +1,4 @@
-import { ToggleControl, SelectControl, ComboboxControl } from '@wordpress/components';
+import { ToggleControl, SelectControl, CheckboxControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { addFilter, applyFilters } from '@wordpress/hooks';
@@ -6,20 +6,63 @@ import { useEffect } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components';
 
-// TODO: Get from Memberful:
-// - All available plans
-// - Any excluded blocks?
+const subscriptionPlans = window?.memberful_wp_block_editor?.options?.memberful_subscriptions || [];
+const excludedBlocks = window?.memberful_wp_block_editor?.block_visibility_excluded_blocks || [];
 
+// Add custom attributes to allowed blocks for visibility controls.
+const MemberfulBlockVisibilityAttributes = (settings, name) => {
+    if (excludedBlocks.includes(name)) {
+        return settings;
+    }
+    if (settings.attributes) {
+        settings.attributes = Object.assign(settings.attributes, {
+            memberfulVisibility: {
+                enum: ['none', 'all', 'active', 'specific'],
+            },
+            memberfulVisibilityHide: {
+                type: 'boolean',
+                default: false,
+            },
+            memberfulVisibilitySpecificPlans: {
+                type: 'array',
+                default: [],
+            },
+        });
+    }
+    return settings;
+};
+
+addFilter('blocks.registerBlockType', 'memberful/block-visibility-attributes', MemberfulBlockVisibilityAttributes);
+
+// Add the visibility controls to the allowed blocks.
 const MemberfulVisibilityControlsOptions = (props) => {
     const { attributes, setAttributes } = props;
-    const { memberfulVisibility, memberfulVisibilityHide, memberfulVisibilitySpecificPlan } = attributes;
+    const { memberfulVisibility, memberfulVisibilityHide, memberfulVisibilitySpecificPlans } = attributes;
+
+    const handleVisibilityChange = (value) => {
+        // Reset the specific plans if any are selected.
+        if (value !== 'specific') {
+            setAttributes({ memberfulVisibilitySpecificPlans: [] });
+        }
+        setAttributes({ memberfulVisibility: value });
+    };
+
+    const toggleVisibilityHide = () => setAttributes({ memberfulVisibilityHide: !memberfulVisibilityHide });
+
+    const handleSpecificPlansChange = (id, value) => {
+        if (value) {
+            setAttributes({ memberfulVisibilitySpecificPlans: [...memberfulVisibilitySpecificPlans, id] });
+        } else {
+            setAttributes({ memberfulVisibilitySpecificPlans: memberfulVisibilitySpecificPlans.filter((planId) => planId !== id) });
+        }
+    };
 
     return (
         <>
             <SelectControl
-                label={__('Visibility', 'memberful')}
+                label={__('Visibility Condition', 'memberful')}
                 value={memberfulVisibility}
-                onChange={(value) => setAttributes({ memberfulVisibility: value })}
+                onChange={handleVisibilityChange}
                 options={[
                     { value: 'none', label: __('None (All users)', 'memberful') },
                     { value: 'all', label: __('All members (active, inactive, or free)', 'memberful') },
@@ -32,40 +75,35 @@ const MemberfulVisibilityControlsOptions = (props) => {
                     <p className="components-base-control__help" style={{ color: 'rgb(117, 117, 117)', fontSize: '12px' }}>
                         {__('Show the block to members with at least one of the following plans:', 'memberful')}
                     </p>
-                    <ComboboxControl
-                        label={__('Select plan(s)', 'memberful')}
-                        options={[
-                            { value: 'plan_1', label: __('Plan 1', 'memberful') },
-                            { value: 'plan_2', label: __('Plan 2', 'memberful') },
-                            { value: 'plan_3', label: __('Plan 3', 'memberful') },
-                        ]}
-                    />
+                    {Object.entries(subscriptionPlans).map(([id, plan]) => (
+                        <CheckboxControl
+                            key={id}
+                            label={plan.name}
+                            checked={memberfulVisibilitySpecificPlans.includes(id)}
+                            onChange={(value) => handleSpecificPlansChange(id, value)}
+                        />
+                    ))}
                 </>
             )}
-            <ToggleControl
-                label={__('Hide when the above conditions are met', 'memberful')}
-                checked={memberfulVisibilityHide}
-                onChange={(value) => setAttributes({ memberfulVisibilityHide: value })}
-            />
+            {memberfulVisibility !== 'none' && (
+                <ToggleControl
+                    label={__('Hide when the above conditions are met', 'memberful')}
+                    checked={memberfulVisibilityHide}
+                    onChange={toggleVisibilityHide}
+                />
+            )}
         </>
     );
 };
 
 const MemberfulVisibilityControls = createHigherOrderComponent((BlockEdit) => {
     return (props) => {
-        const { attributes, setAttributes } = props;
-        const { blockType } = props;
-
-        let excludedBlocks = [];
-
-        excludedBlocks = applyFilters('memberful.excludedBlocksBlockVisibility', excludedBlocks);
-
         return (
             <>
                 <BlockEdit key="edit" {...props} />
-                {!excludedBlocks.includes(blockType) && (
+                {props.isSelected && !excludedBlocks.includes(props.name) && (
                     <InspectorControls>
-                        <PanelBody title="Memberful Visibility">
+                        <PanelBody title={__('Memberful Visibility', 'memberful')}>
                             {MemberfulVisibilityControlsOptions(props)}
                         </PanelBody>
                     </InspectorControls>
--- wordpress/wp-content/plugins/memberful-wp/src/block-editor.php
+++ wordpress/wp-content/plugins/memberful-wp/src/block-editor.php
@@ -2,26 +2,253 @@
 /**
  * Block Editor/Gutenberg compatibility.
  *
+ * @since 1.77.0
+ *
  * @package Memberful
  */
 
+/**
+ * Memberful WP Block Editor Class.
+ *
+ * @package Memberful
+ */
+class Memberful_WP_Block_Editor {
 
-add_action( 'enqueue_block_editor_assets', 'memberful_wp_enqueue_block_editor_assets' );
+	/**
+	 * Class Instance
+	 *
+	 * @var Memberful_WP_Block_Editor
+	 */
+	private static $instance;
+
+	/**
+	 * Get the class instance.
+	 *
+	 * @return Memberful_WP_Block_Editor The class instance.
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_assets' ) );
+
+		if ( ! is_admin() ) {
+			add_action( 'render_block', array( $this, 'render_block' ), 5, 2 );
+		}
+	}
+
+	/**
+	 * Get the blocks that are excluded from the block visibility controls.
+	 *
+	 * @since 1.77.0
+	 *
+	 * @return array The blocks that are excluded from the block visibility controls.
+	 */
+	public static function get_block_visibility_excluded_blocks() {
+
+		/**
+		 * Filters the blocks that are excluded from the block visibility controls.
+		 *
+		 * @since 1.77.0
+		 *
+		 * @param array $block_visibility_excluded_blocks The blocks that are excluded from the block visibility controls.
+		 * @return array The blocks that are excluded from the block visibility controls.
+		*/
+		return apply_filters( 'memberful_wp_block_visibility_excluded_blocks', array() );
+	}
+
+	/**
+	 * Enqueue the block editor assets.
+	 *
+	 * @since 1.77.0
+	 *
+	 * @return void
+	 */
+	public function enqueue_assets(): void {
+		$block_editor_script      = MEMBERFUL_DIR . '/js/build/editor-scripts.asset.php';
+		$block_editor_script_info = file_exists( $block_editor_script )
+			? include $block_editor_script
+			: array(
+				'dependencies' => array(),
+				'version'      => MEMBERFUL_VERSION,
+			);
+
+		wp_enqueue_script(
+			'memberful-wp-block-editor',
+			plugins_url( 'js/build/editor-scripts.js', MEMBERFUL_PLUGIN_FILE ),
+			$block_editor_script_info['dependencies'],
+			$block_editor_script_info['version']
+		);
+		wp_localize_script(
+			'memberful-wp-block-editor',
+			'memberful_wp_block_editor',
+			array(
+				'options'                          => memberful_wp_option_values(),
+				'block_visibility_excluded_blocks' => self::get_block_visibility_excluded_blocks(),
+			)
+		);
+	}
+
+	/**
+	 * Render the block.
+	 *
+	 * @since 1.77.0
+	 *
+	 * @param string $block_content The block content.
+	 * @param array  $block The block.
+	 * @return string The block content.
+	 */
+	public function render_block( $block_content, $block ): string {
+
+		$original_block_content = $block_content;
+
+		// Handle the block visibility conditions.
+		if ( ! empty( $block['attrs']['memberfulVisibility'] ) ) {
+			switch ( $block['attrs']['memberfulVisibility'] ) {
+				case 'all':
+					$block_content = $this->all_members_visibility( $block['attrs'], $block_content );
+					break;
+
+				case 'active':
+					$block_content = $this->active_members_visibility( $block['attrs'], $block_content );
+					break;
+
+				case 'specific':
+					$block_content = $this->specific_members_visibility( $block['attrs'], $block_content );
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		/**
+		 * Filters the block content before it is rendered.
+		 *
+		 * This is run after the block visibility conditions have been applied.
+		 *
+		 * @since 1.77.0
+		 *
+		 * @param string $block_content The block content.
+		 * @param array $block The block.
+		 * @param string $original_block_content The original block content before the visibility conditions were applied.
+		 * @return string The block content.
+		 */
+		$block_content = apply_filters( 'memberful_wp_render_block', $block_content, $block, $original_block_content );
+
+		return $block_content;
+	}
+
+	/**
+	 * All Members Visibility.
+	 *
+	 * Any logged in user will see the block.
+	 *
+	 * @since 1.77.0
+	 *
+	 * @param array $block_attributes The block data.
+	 * @param mixed $block_content The block content.
+	 * @return mixed Returns the new block content.
+	 */
+	public function all_members_visibility( $block_attributes, $block_content ) {
+		if ( ! empty( $block_attributes['memberfulVisibilityHide'] ) && is_user_logged_in() ) {
+			return '';
+		}
+
+		return $block_content;
+	}
+
+	/**
+	 * Active Members Visibility.
+	 *
+	 * Members with any active plan will see the block,
+	 * unless the visibility conditions are reversed.
+	 *
+	 * @since 1.77.0
+	 *
+	 * @param array $block_attributes The block data.
+	 * @param mixed $block_content The block content.
+	 * @return mixed Returns the new block content.
+	 */
+	public function active_members_visibility( $block_attributes, $block_content ) {
+		// Logged out users will not see the block at all.
+		if ( ! is_user_logged_in() ) {
+			return '';
+		}
+
+		if ( $this->should_reverse_visibility_conditions( $block_attributes ) ) {
+			return is_subscribed_to_any_memberful_plan( wp_get_current_user()->ID ) ? '' : $block_content;
+		}
+
+		return is_subscribed_to_any_memberful_plan( wp_get_current_user()->ID ) ? $block_content : '';
+	}
+
+	/**
+	 * Specific Members Visibility.
+	 *
+	 * Members with the specific plans will see the block,
+	 * unless the visibility conditions are reversed or the user does not have any of the specific plans.
+	 *
+	 * @since 1.77.0
+	 *
+	 * @param array $block_attributes The block data.
+	 * @param mixed $block_content The block content.
+	 * @return mixed Returns the new block content.
+	 */
+	public function specific_members_visibility( $block_attributes, $block_content ) {
+		// Logged out users will not see the block at all.
+		if ( ! is_user_logged_in() ) {
+			return '';
+		}
+
+		$plans = $block_attributes['memberfulVisibilitySpecificPlans'] ?? array();
+
+		// Hide the block if the user has any of the specific plans.
+		if ( $this->should_reverse_visibility_conditions( $block_attributes ) ) {
+			return memberful_wp_user_has_subscription_to_plans( wp_get_current_user()->ID, $plans ) ? '' : $block_content;
+		}
+
+		// Show the block if the user has any of the specific plans.
+		return memberful_wp_user_has_subscription_to_plans( wp_get_current_user()->ID, $plans ) ? $block_content : '';
+	}
+
+	/**
+	 * Should the visibility conditions be reversed?
+	 *
+	 * @since 1.77.0
+	 *
+	 * @param array $block_attributes The block data.
+	 * @return boolean Returns true if the visibility conditions should be reversed.
+	 */
+	public function should_reverse_visibility_conditions( $block_attributes ) {
+		return ! empty( $block_attributes['memberfulVisibilityHide'] );
+	}
+}
 
 /**
- * Enqueue block editor assets.
- *
- * These scripts are specifically used for the block editor experience.
- *
- * @return void
+ * Initialize the class.
  */
-function memberful_wp_enqueue_block_editor_assets() {
-  $block_editor_script = include_once MEMBERFUL_DIR . '/js/build/editor-scripts.asset.php';
+Memberful_WP_Block_Editor::get_instance();
 
-  wp_enqueue_script(
-    'memberful-wp-block-editor',
-    plugins_url( 'js/build/editor-scripts.js', MEMBERFUL_PLUGIN_FILE ),
-    $block_editor_script['dependencies'],
-    $block_editor_script['version']
-  );
+
+/**
+ * Helper Functions.
+ */
+
+/**
+ * Get the blocks that are excluded from the block visibility controls.
+ *
+ * @since 1.77.0
+ *
+ * @return array The blocks that are excluded from the block visibility controls.
+ */
+function memberful_wp_get_block_visibility_excluded_blocks() {
+	return Memberful_WP_Block_Editor::get_block_visibility_excluded_blocks();
 }
