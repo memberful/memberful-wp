7d5eb12c35c5e2b903d06cedc19e66ee7e99422c
Lucas Mansur <lucas.mansur2@gmail.com>
2026-05-29 18:13:27 -0300

    Reapply "Merge pull request #404 from TheCodeCompany/feature/MR-7-paywall-location-block" (#408)
    This reverts commit e9629b3e65878fbbba5618cb36cee8330831db4e.

--- wordpress/wp-content/plugins/memberful-wp/src/block-editor.php
+++ wordpress/wp-content/plugins/memberful-wp/src/block-editor.php
@@ -14,242 +14,299 @@
  */
 class Memberful_WP_Block_Editor {
 
-	/**
-	 * Class Instance
-	 *
-	 * @var Memberful_WP_Block_Editor
-	 */
-	private static $instance;
+  /**
+   * Class Instance
+   *
+   * @var Memberful_WP_Block_Editor
+   */
+  private static $instance;
 
-	/**
-	 * Get the class instance.
-	 *
-	 * @return Memberful_WP_Block_Editor The class instance.
-	 */
-	public static function get_instance() {
-		if ( ! isset( self::$instance ) ) {
-			self::$instance = new self();
-		}
-		return self::$instance;
-	}
+  /**
+   * Get the class instance.
+   *
+   * @return Memberful_WP_Block_Editor The class instance.
+   */
+  public static function get_instance() {
+    if ( ! isset( self::$instance ) ) {
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct() {
-		if ( function_exists( 'register_block_type' ) ) {
-			add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_assets' ) );
-			add_filter( 'register_block_type_args', array( $this, 'add_block_visibility_attributes' ), 10, 2 );
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+    if ( function_exists( 'register_block_type' ) ) {
+      add_action( 'init', array( $this, 'register_blocks' ) );
+      add_filter( 'block_categories_all', array( $this, 'register_block_categories' ), 10, 2 );
+      add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_assets' ) );
+      add_filter( 'register_block_type_args', array( $this, 'add_block_visibility_attributes' ), 10, 2 );
 
-			add_action( 'render_block', array( $this, 'render_block' ), 10, 2 );
-		}
-	}
+      add_action( 'render_block', array( $this, 'render_block' ), 10, 2 );
+    }
+  }
 
-	/**
-	 * Get the blocks that are excluded from the block visibility controls.
-	 *
-	 * @since 1.77.0
-	 *
-	 * @return array The blocks that are excluded from the block visibility controls.
-	 */
-	public static function get_block_visibility_excluded_blocks() {
+  /**
+   * Get the blocks that are excluded from the block visibility controls.
+   *
+   * @since 1.77.0
+   *
+   * @return array The blocks that are excluded from the block visibility controls.
+   */
+  public static function get_block_visibility_excluded_blocks() {
 
-		$excluded_blocks = array();
+    $excluded_blocks = array(
+      'memberful/paywall-divider',
+    );
 
-		/**
-		 * Filters the blocks that are excluded from the block visibility controls.
-		 *
-		 * @since 1.77.0
-		 *
-		 * @param array $block_visibility_excluded_blocks The blocks that are excluded from the block visibility controls.
-		 * @return array The blocks that are excluded from the block visibility controls.
-		*/
-		return apply_filters( 'memberful_wp_block_visibility_excluded_blocks', $excluded_blocks );
-	}
+    /**
+     * Filters the blocks that are excluded from the block visibility controls.
+     *
+     * @since 1.77.0
+     *
+     * @param array $block_visibility_excluded_blocks The blocks that are excluded from the block visibility controls.
+     * @return array The blocks that are excluded from the block visibility controls.
+    */
+    return apply_filters( 'memberful_wp_block_visibility_excluded_blocks', $excluded_blocks );
+  }
 
-	/**
-	 * Enqueue the block editor assets.
-	 *
-	 * @since 1.77.0
-	 *
-	 * @return void
-	 */
-	public function enqueue_assets(): void {
-		$block_editor_script      = MEMBERFUL_DIR . '/js/build/editor-scripts.asset.php';
-		$block_editor_script_info = file_exists( $block_editor_script )
-			? include $block_editor_script
-			: array(
-				'dependencies' => array(),
-				'version'      => MEMBERFUL_VERSION,
-			);
+  /**
+   * Register Memberful block types.
+   *
+   * @since 1.78.1
+   *
+   * @return void
+   */
+  public function register_blocks(): void {
+    $block_directory = MEMBERFUL_DIR . '/js/build/blocks/paywall-divider';
 
-		wp_enqueue_script(
-			'memberful-wp-block-editor',
-			plugins_url( 'js/build/editor-scripts.js', MEMBERFUL_PLUGIN_FILE ),
-			$block_editor_script_info['dependencies'],
-			$block_editor_script_info['version']
-		);
-		wp_localize_script(
-			'memberful-wp-block-editor',
-			'memberful_wp_block_editor',
-			array(
-				'options'                          => memberful_wp_option_values(),
-				'block_visibility_excluded_blocks' => self::get_block_visibility_excluded_blocks(),
-			)
-		);
-	}
+    if ( ! file_exists( $block_directory . '/block.json' ) ) {
+      $block_directory = MEMBERFUL_DIR . '/js/src/blocks/paywall-divider';
+    }
 
-	/**
-	 * Add the block visibility attributes to registered blocks server-side.
-	 *
-	 * @since 1.77.0
-	 *
-	 * @param array  $args The block arguments.
-	 * @param string $name The block name.
-	 * @return array The block arguments.
-	 */
-	public function add_block_visibility_attributes( $args, $name ) {
-		// Skip for excluded blocks.
-		if ( in_array( $name, self::get_block_visibility_excluded_blocks(), true ) ) {
-			return $args;
-		}
+    register_block_type(
+      $block_directory,
+      array(
+        'render_callback' => 'memberful_wp_render_paywall_divider_block',
+      )
+    );
+  }
 
-		$args['attributes']['memberful_visibility'] = array(
-			'type'    => 'string',
-			'enum'    => array( 'none', 'logged_in', 'specific' ),
-			'default' => 'none',
-		);
+  /**
+   * Register custom block categories for the block inserter.
+   *
+   * @since 1.78.1
+   *
+   * @param array $categories Existing block categories.
+   * @return array Updated block categories.
+   */
+  public function register_block_categories( $categories ) {
+    $memberful_category_exists = false;
 
-		$args['attributes']['memberful_visibility_hide'] = array(
-			'type'    => 'boolean',
-			'default' => false,
-		);
+    foreach ( $categories as $category ) {
+      if ( isset( $category['slug'] ) && 'memberful' === $category['slug'] ) {
+        $memberful_category_exists = true;
+        break;
+      }
+    }
 
-		$args['attributes']['memberful_visibility_plans'] = array(
-			'type'    => 'array',
-			'default' => array(),
-		);
+    if ( $memberful_category_exists ) {
+      return $categories;
+    }
 
-		return $args;
-	}
+    $categories[] = array(
+      'slug'  => 'memberful',
+      'title' => __( 'Memberful', 'memberful' ),
+      'icon'  => null,
+    );
 
-	/**
-	 * Conditionally render the block based on the block visibility conditions.
-	 *
-	 * @since 1.77.0
-	 *
-	 * @param string $block_content The block content.
-	 * @param array  $block The block.
-	 * @return string The block content.
-	 */
-	public function render_block( $block_content, $block ): string {
+    return $categories;
+  }
 
-		$original_block_content = $block_content;
+  /**
+   * Enqueue the block editor assets.
+   *
+   * @since 1.77.0
+   *
+   * @return void
+   */
+  public function enqueue_assets(): void {
+    $block_editor_script      = MEMBERFUL_DIR . '/js/build/editor-scripts.asset.php';
+    $block_editor_script_info = file_exists( $block_editor_script )
+      ? include $block_editor_script
+      : array(
+        'dependencies' => array(),
+        'version'      => MEMBERFUL_VERSION,
+      );
 
-		// Skip if no visibility rule is set.
-		if ( empty( $block['attrs'] ) || empty( $block['attrs']['memberful_visibility'] ) ) {
-			return $block_content;
-		}
+    wp_enqueue_script(
+      'memberful-wp-block-editor',
+      plugins_url( 'js/build/editor-scripts.js', MEMBERFUL_PLUGIN_FILE ),
+      $block_editor_script_info['dependencies'],
+      $block_editor_script_info['version']
+    );
+    wp_localize_script(
+      'memberful-wp-block-editor',
+      'memberful_wp_block_editor',
+      array(
+        'options'                          => memberful_wp_option_values(),
+        'block_visibility_excluded_blocks' => self::get_block_visibility_excluded_blocks(),
+      )
+    );
+  }
 
-		// Handle the block visibility conditions.
-		switch ( $block['attrs']['memberful_visibility'] ) {
-			case 'logged_in':
-				$block_content = $this->all_members_visibility( $block['attrs'], $block_content );
-				break;
+  /**
+   * Add the block visibility attributes to registered blocks server-side.
+   *
+   * @since 1.77.0
+   *
+   * @param array  $args The block arguments.
+   * @param string $name The block name.
+   * @return array The block arguments.
+   */
+  public function add_block_visibility_attributes( $args, $name ) {
+    // Skip for excluded blocks.
+    if ( in_array( $name, self::get_block_visibility_excluded_blocks(), true ) ) {
+      return $args;
+    }
 
-			case 'specific':
-				$block_content = $this->specific_plans_visibility( $block['attrs'], $block_content );
-				break;
+    $args['attributes']['memberful_visibility'] = array(
+      'type'    => 'string',
+      'enum'    => array( 'none', 'logged_in', 'specific' ),
+      'default' => 'none',
+    );
 
-			default:
-				break;
-		}
+    $args['attributes']['memberful_visibility_hide'] = array(
+      'type'    => 'boolean',
+      'default' => false,
+    );
 
-		/**
-		 * Filters the block content before it is rendered.
-		 *
-		 * This is run after the block visibility conditions have been applied.
-		 *
-		 * @since 1.77.0
-		 *
-		 * @param string $block_content The block content.
-		 * @param array $block The block.
-		 * @param string $original_block_content The original block content before the visibility conditions were applied.
-		 * @return string The block content.
-		 */
-		$block_content = apply_filters( 'memberful_wp_render_block', $block_content, $block, $original_block_content );
+    $args['attributes']['memberful_visibility_plans'] = array(
+      'type'    => 'array',
+      'default' => array(),
+    );
 
-		return $block_content;
-	}
+    return $args;
+  }
 
-	/**
-	 * All Members Visibility.
-	 *
-	 * Any logged in user will see the block.
-	 *
-	 * @since 1.77.0
-	 *
-	 * @param array $block_attributes The block data.
-	 * @param mixed $block_content The block content.
-	 * @return mixed Returns the new block content.
-	 */
-	public function all_members_visibility( $block_attributes, $block_content ) {
-		$is_logged_in = is_user_logged_in();
+  /**
+   * Conditionally render the block based on the block visibility conditions.
+   *
+   * @since 1.77.0
+   *
+   * @param string $block_content The block content.
+   * @param array  $block The block.
+   * @return string The block content.
+   */
+  public function render_block( $block_content, $block ): string {
 
-		if ( $this->should_reverse_visibility_conditions( $block_attributes ) ) {
-			// Show to logged out users only.
-			return $is_logged_in ? '' : $block_content;
-		}
+    $original_block_content = $block_content;
 
-		// Default: show to any logged‑in user only.
-		return $is_logged_in ? $block_content : '';
-	}
+    // Skip if no visibility rule is set.
+    if ( empty( $block['attrs'] ) || empty( $block['attrs']['memberful_visibility'] ) ) {
+      return $block_content;
+    }
 
-	/**
-	 * Specific Plans Visibility.
-	 *
-	 * Members with the specific plans will see the block,
-	 * unless the visibility conditions are reversed or the user does not have any of the specific plans.
-	 *
-	 * @since 1.77.0
-	 *
-	 * @param array $block_attributes The block data.
-	 * @param mixed $block_content The block content.
-	 * @return mixed Returns the new block content.
-	 */
-	public function specific_plans_visibility( $block_attributes, $block_content ) {
-		// Logged out users will not see the block at all.
-		if ( ! is_user_logged_in() ) {
-			return '';
-		}
+    // Handle the block visibility conditions.
+    switch ( $block['attrs']['memberful_visibility'] ) {
+      case 'logged_in':
+        $block_content = $this->all_members_visibility( $block['attrs'], $block_content );
+        break;
 
-		$plans = $block_attributes['memberful_visibility_plans'] ?? array();
+      case 'specific':
+        $block_content = $this->specific_plans_visibility( $block['attrs'], $block_content );
+        break;
 
-		// No plans configured - fall back to rendering the block unmodified.
-		if ( empty( $plans ) ) {
-			return $block_content;
-		}
+      default:
+        break;
+    }
 
-		// Hide the block if the user has any of the specific plans.
-		if ( $this->should_reverse_visibility_conditions( $block_attributes ) ) {
-			return memberful_wp_user_has_subscription_to_plans( wp_get_current_user()->ID, $plans ) ? '' : $block_content;
-		}
+    /**
+     * Filters the block content before it is rendered.
+     *
+     * This is run after the block visibility conditions have been applied.
+     *
+     * @since 1.77.0
+     *
+     * @param string $block_content The block content.
+     * @param array $block The block.
+     * @param string $original_block_content The original block content before the visibility conditions were applied.
+     * @return string The block content.
+     */
+    $block_content = apply_filters( 'memberful_wp_render_block', $block_content, $block, $original_block_content );
 
-		// Show the block if the user has any of the specific plans.
-		return memberful_wp_user_has_subscription_to_plans( wp_get_current_user()->ID, $plans ) ? $block_content : '';
-	}
+    return $block_content;
+  }
 
-	/**
-	 * Should the visibility conditions be reversed?
-	 *
-	 * @since 1.77.0
-	 *
-	 * @param array $block_attributes The block data.
-	 * @return boolean Returns true if the visibility conditions should be reversed.
-	 */
-	public function should_reverse_visibility_conditions( $block_attributes ) {
-		return ! empty( $block_attributes['memberful_visibility_hide'] );
-	}
+  /**
+   * All Members Visibility.
+   *
+   * Any logged in user will see the block.
+   *
+   * @since 1.77.0
+   *
+   * @param array $block_attributes The block data.
+   * @param mixed $block_content The block content.
+   * @return mixed Returns the new block content.
+   */
+  public function all_members_visibility( $block_attributes, $block_content ) {
+    $is_logged_in = is_user_logged_in();
+
+    if ( $this->should_reverse_visibility_conditions( $block_attributes ) ) {
+      // Show to logged out users only.
+      return $is_logged_in ? '' : $block_content;
+    }
+
+    // Default: show to any logged‑in user only.
+    return $is_logged_in ? $block_content : '';
+  }
+
+  /**
+   * Specific Plans Visibility.
+   *
+   * Members with the specific plans will see the block,
+   * unless the visibility conditions are reversed or the user does not have any of the specific plans.
+   *
+   * @since 1.77.0
+   *
+   * @param array $block_attributes The block data.
+   * @param mixed $block_content The block content.
+   * @return mixed Returns the new block content.
+   */
+  public function specific_plans_visibility( $block_attributes, $block_content ) {
+    // Logged out users will not see the block at all.
+    if ( ! is_user_logged_in() ) {
+      return '';
+    }
+
+    $plans = $block_attributes['memberful_visibility_plans'] ?? array();
+
+    // No plans configured - fall back to rendering the block unmodified.
+    if ( empty( $plans ) ) {
+      return $block_content;
+    }
+
+    // Hide the block if the user has any of the specific plans.
+    if ( $this->should_reverse_visibility_conditions( $block_attributes ) ) {
+      return memberful_wp_user_has_subscription_to_plans( wp_get_current_user()->ID, $plans ) ? '' : $block_content;
+    }
+
+    // Show the block if the user has any of the specific plans.
+    return memberful_wp_user_has_subscription_to_plans( wp_get_current_user()->ID, $plans ) ? $block_content : '';
+  }
+
+  /**
+   * Should the visibility conditions be reversed?
+   *
+   * @since 1.77.0
+   *
+   * @param array $block_attributes The block data.
+   * @return boolean Returns true if the visibility conditions should be reversed.
+   */
+  public function should_reverse_visibility_conditions( $block_attributes ) {
+    return ! empty( $block_attributes['memberful_visibility_hide'] );
+  }
 }
 
 /**
@@ -270,5 +327,14 @@ Memberful_WP_Block_Editor::get_instance();
  * @return array The blocks that are excluded from the block visibility controls.
  */
 function memberful_wp_get_block_visibility_excluded_blocks() {
-	return Memberful_WP_Block_Editor::get_block_visibility_excluded_blocks();
+  return Memberful_WP_Block_Editor::get_block_visibility_excluded_blocks();
+}
+
+/**
+ * Render callback for the Memberful paywall divider block.
+ *
+ * @return string
+ */
+function memberful_wp_render_paywall_divider_block() {
+  return '<!-- memberful-paywall-divider -->';
 }
